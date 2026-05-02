8124681718bdc05de6956104c104332d88cddf7a
Babuali7739 <129478089+Babuali7739@users.noreply.github.com>
2026-05-02 13:57:34 +0530

    error2

--- index.js
+++ index.js
@@ -22,9 +22,12 @@ connectDB()
 app.use(cookieParser())
 app.use(express.json())
 app.use(cors({
-      origin: ["https://team-task-manager-rcx2.vercel.app", "http://localhost:5173"],
+      origin: ["https://team-task-manager-liart-phi.vercel.app",
+  "https://team-task-manager-iota-one.vercel.app", "http://localhost:5173"],
       credentials: true
 }))
+
+app.options("*", cors());
 
 // API Endpoints
 app.use("/api/user", userRouter)

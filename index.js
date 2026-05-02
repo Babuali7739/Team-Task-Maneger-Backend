96e31d9df92d096c7d44600bc0c284aafacdd213
Babuali7739 <129478089+Babuali7739@users.noreply.github.com>
2026-05-02 14:05:29 +0530

    test

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ connectDB()
 app.use(cookieParser())
 app.use(express.json())
 app.use(cors({
-      origin: ["https://team-task-manager-iota-one.vercel.app/", "http://localhost:5173"],
+      origin: ["https://team-task-manager-iota-one.vercel.app", "http://localhost:5173"],
       credentials: true
 }))
 

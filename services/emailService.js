8bd7f027abf4de4b14d2808f7af2fb90c1d4d17e
Babuali7739 <129478089+Babuali7739@users.noreply.github.com>
2026-05-02 14:25:39 +0530

    email

--- services/emailService.js
+++ services/emailService.js
@@ -4,10 +4,12 @@ dotenv.config()
 import nodemailer from "nodemailer"
 
 export const transporter = nodemailer.createTransport({
-      host: "smtp-relay.brevo.com",
-      port: 587,
-      auth: {
-            user: process.env.SMTP_USER,
-            pass: process.env.SMTP_PASS
-      }
-})
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false, // true for 465
+  auth: {
+    user: process.env.SENDER_EMAIL,
+    pass: process.env.SENDER_PASS,
+  },
+  connectionTimeout: 10000, // optional
+});
